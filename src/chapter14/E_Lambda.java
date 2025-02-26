d6aaa2ae2f5f876171d1191ff02a589f21906784
chang0506 <j8970@naver.com>
2025-02-26 10:00:56 +0900

    0226 - lambdaPrac

--- src/chapter14/E_Lambda.java
+++ src/chapter14/E_Lambda.java
@@ -22,22 +22,21 @@ import java.util.function.Supplier;
 
 // cf) negate(부정하다): 현재의 결과를 역전
 
-
-// 2. Function<T, R>
+// 2. Function<T, R>(실행하다)
 // : 입력 값을 받아 특정 연산을 수행한 후 결과를 반환
-//: 메서드
+// : 메서드
 //		- R apply(T t): 입력값을 받아 변환한 값을 반환
 //		- andThen(Function after): 현재 결과를 다른 Function의 입력으로 연결
 //		- compose(Function before): 다른 Function의 결과를 현재 입력으로 연결
 
 // 3. Consumer<T>(소비하다)
-// : 입력 값을 받아 소비(출력 또는 상태를 변경)하는데 사용
+// : 입력 값을 받아 소비(출력 또는 상태 변경)하는 데 사용
 // : 메서드
 //		- void accept(T t): 입력값을 소비
 //		- andThen(Consumer after): 연속적인 소비 작업을 위해 사용
 
 // 4. Supplier<T>(공급하다)
-// : 값을 공급(생성)하는 데 사용, 입력 값을 필요로 하지 X
+// : 값을 공급(생성)하는 데 사용, 입력 값이 필요로 하지 X
 // : 외부에서 값을 가져오거나, 데이터를 생성하여 반환하는 역할
 // : 메서드
 //		- T get(): 반환
@@ -54,18 +53,18 @@ public class E_Lambda {
 		/*
 		 * @FunctionalInterface
 		 * interface Predicate<T> {
-		 * 	boolean test(T t);
-		 * }
-		 */
+		 * 		boolean test(T t);
+		 * } 
+		 * */
 		
 		// 기본 조건 검사
-		System.out.println(isEven.test(10)); // t
-		System.out.println(isPositive.test(-10)); // f
+		System.out.println(isEven.test(10)); // true
+		System.out.println(isPositive.test(-10)); // false
 		
 		// 짝수이면서 양수인 경우를 검사
 		Predicate<Integer> isEvenAndPositive = isEven.and(isPositive);
-		System.out.println(isEvenAndPositive.test(4)); // t
-		System.out.println(isEvenAndPositive.test(-4)); // f
+		System.out.println(isEvenAndPositive.test(4)); // true
+		System.out.println(isEvenAndPositive.test(-4)); // false
 		
 		// == Function == //
 		System.out.println("== Function ==");
@@ -75,35 +74,37 @@ public class E_Lambda {
 		 * interface Function<T, R> {
 		 * 		R apply(T t);
 		 * }
-		 */
+		 * */
 		Function<String, Integer> stringLength = s -> s.length();
 		Function<Integer, Integer> square = n -> n * n;
 		Function<String, String> upperString = s -> s.toUpperCase();
 		
-		System.out.println(stringLength.apply("테스트 코드"));
+		System.out.println(stringLength.apply("이도경 메롱"));
 		System.out.println(upperString.apply("nice to meet you"));
 		
 		Function<String, Integer> lengthAndSquare = stringLength.andThen(square);
 		// : 문자열의 길이(숫자)값을 인자로 받아 계산
 		System.out.println(lengthAndSquare.apply("이 문자열 길이의 제곱값은?"));
 		
+		
+	
 		// == Consumer == //
 		System.out.println("== Consumer ==");
 		
 		/*
-		 * @FunctionalInterface
+		 * @FuntionalInterface
 		 * interface Consumer<T> {
 		 * 		void accept(T t);
 		 * }
-		 */
+		 * */
 		Consumer<String> printMessage = msg -> System.out.println(msg);
 		Consumer<String> printLength = msg -> System.out.println(msg.length());
 		
-		printMessage.accept("안녕하세요");
-		printLength.accept("123456");
+		printMessage.accept("안녕하세요 :)");
+		printLength.accept("1234567"); // 7
 		
 		Consumer<String> combinedConsumer = printMessage.andThen(printLength);
-		combinedConsumer.accept("123");
+		combinedConsumer.accept("123"); // 3
 		
 		// == Supplier == //
 		System.out.println("== Supplier ==");
@@ -113,43 +114,18 @@ public class E_Lambda {
 		 * interface Supplier<T> {
 		 * 		T get();
 		 * }
-		 */
+		 * */
+		
 		// Math.random(): 0.0과 1.0 사이의 무작위 실수를 반환
 		Supplier<Double> randomValue = () -> Math.random();
 		System.out.println(randomValue.get());
 		
 		// == Map 활용 람다식 == //
 		Map<String, Integer> scores = new HashMap<>();
-		scores.put("전창현", 90);
-		scores.put("문창배", 100);
-		scores.put("모르는사람", 95);
+		scores.put("이승아", 90);
+		scores.put("이도경", 100);
+		scores.put("이지희", 95);
 		
 		scores.forEach((name, score) -> System.out.println(name + ": " + score));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 }
--- src/chapter14/G_Stream.java
+++ src/chapter14/G_Stream.java
@@ -14,11 +14,10 @@ import java.util.stream.Stream;
 // - 함수형 프로그래밍 기법과 함께 사용
 
 // == 1. 특징 == //
-
-// 1) 스트림은 데이터를 소스로부터 연속적으로 처리하는 '파이프 라인'을 구축
+// 1) 스트림은 데이터를 소스로 부터 연속적으로 처리하는 '파이프 라인'을 구축
 
 // cf) 데이터 파이프 라인
-// 데이터를 사용하는 단계, 절차, 흐름
+// : 데이터를 사용하는 단계, 절차, 흐름
 
 // 2) 데이터 변경 X (불변성)
 // : 스트림은 원본 데이터 변경 X, 새로운 값을 반환
@@ -35,23 +34,24 @@ import java.util.stream.Stream;
 // : 데이터를 필터링, 매핑(변환)하는 과정
 // - 연산은 연속적으로 사용 가능
 // - 스트림은 새로운 스트림을 반환
-// EX) filter, map, sorted 등
+// EX) filer, map, sorted 등
 
 // 3) 최종 연산
 // : 모든 데이터를 처리한 후 결과를 반환하거나 출력하는 연산
 // EX) collect, forEach, count 등
 
+
 public class G_Stream {
 	public static void main(String[] args) {
 		List<String> names = new ArrayList<String>();
 		
-		names.add("전창현1");
-		names.add("전창현2");
-		names.add("전창현3");
-		names.add("문창배1");
-		names.add("문창배2");
+		names.add("이승아");
+		names.add("조승범");
+		names.add("진상영");
+		names.add("이은우");
+		names.add("윤안나");
 		
-		System.out.println(names); // [전창현1, 전창현2, 전창현3, 전창현4, 전창현5]
+		System.out.println(names); // [이승아, 조승범, 진상영, 이은우, 윤안나]
 		
 		// 1. 데이터 소스(Stream 생성)
 		// 데이터.stream()
@@ -60,37 +60,38 @@ public class G_Stream {
 		// : 스트림.키워드(람다식)
 		// >> 스트림 소스에 원하는 기능을 사용
 		// >> 전체 스트림을 순회하여 각 키워드의 기능을 수행
-		
+
 		// cf) 스트림 중간 연산 내부의 람다식
 		//		: 스트림 내부의 요소값을 하나씩 매개변수에 담음
-		// 		>> 구현부에서 인자로 해당 매개변수와 일치하는 값을 전달하는 경우 메서드 참조 가능
+		//		>> 구현부에서 인자로 해당 매개변수와 일치하는 값을 전달하는 경우 메서드 참조 가능
 		
 		// - filter: 조건에 맞는 요소만 선택
 		// - map: 요소 변환
-		// - sorted: 요소 정렬 
+		// - sorted: 요소 정렬
 		// - distinct: 중복 제거
 		// - limit: 처음부터 지정된 개수만큼 선택
 		// - skip: 처음부터 지정된 개수만큼 건너뜀
 		
 		// 3. 최종 연산
-		// - forEach: 각 요소를 처리(반환 X)
+		// - forEach: 각 요소를 처리 (반환 X)
 		// - collect: 요소를 변환하여 컬렉션으로 반환
 		// - reduce: 모든 요소를 하나의 값으로 줄임
 		// - count: 요소의 개수를 반환
+		
 		names.stream()
 			.map(name -> name + "님")
-			// 문자열.startWith(""): 해당 키워드로 시작하는 문자일 경우 true를 반환
-			.filter(name -> name.startsWith("전"))
-			// .forEach(System.out::print);
-			.forEach(name -> System.out.println(name));		
+			// 문자열.startsWith(""): 해당 키워드로 시작하는 문자일 경우 true를 반환
+			.filter(name -> name.startsWith("이")) 
+//			.forEach(System.out::println);
+			.forEach(name -> System.out.println(name));
 		
 		// Arrays.asList();
 		// : 일반 배열을 ArrayList로 반환
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 		
 		List<Integer> newNumbers = numbers.stream()
-											.map(n -> n * n)
-											.collect(Collectors.toList());
+				.map(n -> n * n)
+				.collect(Collectors.toList());
 		
 		// cf) .stream으로 생성된 스트림은 일회성으로만 사용 가능
 		//		: 메서드 체이닝으로 여러 기능을 적용
@@ -103,7 +104,7 @@ public class G_Stream {
 //		stream.filter(n -> n % 2 == 0)
 //			.collect(Collectors.toList());
 		// >> java.lang.IllegalStateException 예외 발생
-		// : 스트림을 재사용하는 경우 예외 발생
+		//		: 스트림을 재사용하는 경우 예외 발생
 		
 		System.out.println(numbers); // [1, 2, 3, 4, 5]
 		System.out.println(newNumbers); // [1, 4, 9, 16, 25] - 불변성!
