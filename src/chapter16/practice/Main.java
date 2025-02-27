8dfe81a612cc670eb54d85f599299a66b2ad4100
chang0506 <j8970@naver.com>
2025-02-27 12:46:12 +0900

    0227 - bookPrac

---
+++ src/chapter16/practice/Main.java
@@ -0,0 +1,11 @@
+package chapter16.practice;
+
+// = 도서 관리 시스템 = //
+// : 도서 카테고리 ENUM 자료형 추가
+// >> 조회 기능(카테고리별 검색 - 필터링)
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
